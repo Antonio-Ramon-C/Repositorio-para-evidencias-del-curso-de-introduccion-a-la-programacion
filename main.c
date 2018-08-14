432ed12dbadbbf3337d5f0fb9d9942368939f2d7
Antonio-Ramon-C <42361700+Antonio-Ramon-C@users.noreply.github.com>
2018-08-13 20:36:49 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h> /*printf(Const char*,...)*/
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  putchar('A');
+  putchar('\n');
+  printf("Hola mundo C!!!\n");
+  system("PAUSE");	
+  return 0;
+}
